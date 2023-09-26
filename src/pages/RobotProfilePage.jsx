18892a10dc74c66400511342ade02998de08e3a8
Yi-Ting Li <liyitingtom@gmail.com>
2023-09-26 12:41:59 +0800

    feat: waitting screen for RobotProfile page

--- src/pages/RobotProfilePage.jsx
+++ src/pages/RobotProfilePage.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { BiUserPlus } from 'react-icons/bi';
+import { Rings } from 'react-loader-spinner';
 import { Row, Card, Button, Col } from 'reactstrap';
 import { styled } from 'styled-components';
 
 import { getRobotProfiles } from '../api/robotProfile';
 import RobotProfileCard from '../components/RobotProfileCard/RobotProfileCard';
 import RobotProfileCreationCard from '../components/RobotProfileCard/RobotProfileCreationCard';
+import { color } from '../style';
 
 const ProfileCard = styled(Card)`
   border-radius: 1em;
@@ -24,6 +26,7 @@ const AddRobotButton = styled(Button)`
 function RobotProfilePage() {
   const [emhRobots, setEmhRobots] = useState([]);
   const [toggle, setToggle] = useState(false);
+  const [isFetchingData, setIsFetchingData] = useState(true);
 
   const fetchRobotsProfiles = async () => {
     const emhRobotsInDB = await getRobotProfiles();
@@ -35,40 +38,70 @@ function RobotProfilePage() {
     fetchRobotsProfiles();
   }, []);
 
+  useEffect(() => {
+    if (emhRobots.length > 0) {
+      setIsFetchingData(false);
+    }
+  }, [emhRobots]);
+
   return (
     <div className="content">
-      <Row>
-        {emhRobots.length > 0 &&
-          emhRobots.map((robot) => (
-            <RobotProfileCard
-              {...robot}
-              key={robot.name}
-              refetchRobotsProfiles={fetchRobotsProfiles}
-            />
-          ))}
-
-        <Col md="6" lg="4">
-          <ProfileCard className="card-user">
-            <AddRobotButton
-              className="btn-primary"
-              onClick={() => setToggle(!toggle)}
-            >
-              <BiUserPlus size="5em" />
-            </AddRobotButton>
-          </ProfileCard>
-          <RobotProfileCreationCard
-            refetchRobotsProfiles={fetchRobotsProfiles}
-            toggle={toggle}
-            setToggle={setToggle}
-            defaultExtra={''}
-            defaultImageURL={''}
-            defaultName={''}
-            defaultOptions={[]}
-            defaultPersonality={''}
-            defaultStatus={'CREATE'}
+      {isFetchingData ? (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '60vh', // Adjust as needed
+          }}
+        >
+          <Rings
+            height="30vh"
+            width="30vh"
+            color={color.primaryA(0.6)}
+            radius="6"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel="rings-loading"
           />
-        </Col>
-      </Row>
+          <h1>Fetching Data...</h1>
+        </div>
+      ) : (
+        <Row>
+          {emhRobots.length > 0 &&
+            emhRobots.map((robot) => (
+              <RobotProfileCard
+                {...robot}
+                key={robot.name}
+                refetchRobotsProfiles={fetchRobotsProfiles}
+              />
+            ))}
+
+          <Col md="6" lg="4">
+            <ProfileCard className="card-user">
+              <AddRobotButton
+                className="btn-primary"
+                onClick={() => setToggle(!toggle)}
+              >
+                <BiUserPlus size="5em" />
+              </AddRobotButton>
+            </ProfileCard>
+            <RobotProfileCreationCard
+              refetchRobotsProfiles={fetchRobotsProfiles}
+              toggle={toggle}
+              setToggle={setToggle}
+              defaultExtra={''}
+              defaultImageURL={''}
+              defaultName={''}
+              defaultOptions={[]}
+              defaultPersonality={''}
+              defaultStatus={'CREATE'}
+            />
+          </Col>
+        </Row>
+      )}
     </div>
   );
 }
