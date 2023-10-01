0cc11a30b59f2c236b08c7ce3e16f7e310938f77
Yi-Ting Li <liyitingtom@gmail.com>
2023-10-01 16:56:51 +0800

    feat: unify the API structure

--- src/api/chat.js
+++ src/api/chat.js
@@ -17,32 +17,15 @@ export async function fetchAllChatsByUserId(userId) {
   }
 }
 
-export async function sendChatMessage(userId, username, message, parentId) {
-  console.debug(
-    'Sending chat message:',
-    message,
-    'for user:',
-    userId,
-    'with parent ID:',
-    parentId,
-    'and username:',
-    username,
-  );
+export async function sendChatMessage(chatData, robotProfile) {
+  console.debug('Sending chat data:', chatData);
+  console.debug('Sending robot profile:', robotProfile);
   try {
     const response = await axios.post(
       `${baseUrl}/chat`,
       {
-        chat: {
-          user_id: userId,
-          username: username,
-          request: message,
-          parent_id: parentId,
-        },
-        format_dict: {
-          model_name: 'Alpha Noble',
-          model_personality:
-            'practical, sincere, talkative, thoughtful and considerate',
-        },
+        chat_data: chatData,
+        robot_profile: robotProfile,
       },
       {
         headers: { 'Content-Type': 'application/json' },
@@ -54,6 +37,6 @@ export async function sendChatMessage(userId, username, message, parentId) {
     }
   } catch (err) {
     // Log and re-throw the error to be handled by the caller
-    console.error('Error sending chat message:', message, err);
+    console.error('Error sending chat message:', chatData, err);
   }
 }
